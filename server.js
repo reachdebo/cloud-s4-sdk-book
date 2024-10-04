079254ebde5a285ab7f6aadb7b5e80c2df1d956c
Debopriya Ghosh <debopriya.ghosh@capgemini.com>
2024-10-04 10:44:50 +0000

    Initial Commit

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require('./app');
 
 const nodeAppStarted = Date.now();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8081;
 app.listen(port, () => console.log(`Mock server started on port ${port} after ${Date.now() - nodeAppStarted} ms, running - stop with CTRL+C (or CMD+C)...`))
